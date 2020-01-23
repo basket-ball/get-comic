1990dcc0e4065a082c6adb1c05c0db0739a9a9d2
basket_ball <53385583+basket-ball@users.noreply.github.com>
2020-01-23 15:21:06 +0800

    Update getComic.py

--- getComic.py
+++ getComic.py
@@ -1,4 +1,4 @@
-import requests,re,time,os,threading,multiprocessing,sys,tkinter
+import requests,re,time,os,threading,multiprocessing,sys
 from bs4 import BeautifulSoup
 header={'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36 Edg/79.0.309.63'}
 class bnComic:
